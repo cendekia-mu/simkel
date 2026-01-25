db13e8568be901df5b20620708ddea27ec11413e
aa.gustiana@gmail.com <aa.gustiana@gmail.com>
2026-01-25 15:59:26 +0700

    Remove PermohonantTypeModel and PermohonanModel from __init__.py; add new models in permohonan.py

--- simkel/models/__init__.py
+++ simkel/models/__init__.py
@@ -16,22 +16,6 @@ register(SimkelDBSession)
 class StandarModel(models.StandarModel):
     db_session = SimkelDBSession
 
-class PermohonantTypeModel(StandarModel, SimkelBase):
-    __tablename__ = 'permohonan_type'
-    name = Column(String(255))
-    level=Column(Integer)  # e.g., 1 for high, 2 for medium, etc.
-    def __repr__(self):
-        return f"<PermohonantTypeModel(name={self.name}, description={self.description})>"
-    
-class PermohonanModel(StandarModel, SimkelBase):
-    __tablename__ = 'permohonan'
-    id = Column(Integer, primary_key=True)
-    nomor = Column(String(50))
-    jenis_id = Column(Integer, ForeignKey('permohonan_type.id'))
-    jenis = relationship("PermohonantTypeModel")
-    pemohon_nama = Column(String(255))
-    pemohon_alamat = Column(Text)
-    status = Column(String(50))  # e.g., 'pending', 'approved', 'rejected'
+class User(models.User, SimkelBase):
+    db_session = SimkelDBSession
 
-    def __repr__(self):
-        return f"<PermohonanModel(nomor={self.nomor}, pemohon_nama={self.pemohon_nama}, status={self.status})>"
---
+++ simkel/models/permohonan.py
@@ -0,0 +1,26 @@
+
+from . import SimkelBase, StandarModel
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+
+class PermohonantTypeModel(StandarModel, SimkelBase):
+    __tablename__ = 'permohonan_type'
+    name = Column(String(255))
+    level = Column(Integer)  # e.g., 1 for high, 2 for medium, etc.
+
+    def __repr__(self):
+        return f"<PermohonantTypeModel(name={self.name}, description={self.description})>"
+
+
+class PermohonanModel(StandarModel, SimkelBase):
+    __tablename__ = 'permohonan'
+    id = Column(Integer, primary_key=True)
+    nomor = Column(String(50))
+    jenis_id = Column(Integer, ForeignKey('permohonan_type.id'))
+    jenis = relationship("PermohonantTypeModel")
+    pemohon_nama = Column(String(255))
+    pemohon_alamat = Column(Text)
+    status = Column(String(50))  # e.g., 'pending', 'approved', 'rejected'
+
+    def __repr__(self):
+        return f"<PermohonanModel(nomor={self.nomor}, pemohon_nama={self.pemohon_nama}, status={self.status})>"
